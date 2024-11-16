8bc68cd10bb477098ee69a253ed26e8bcecd8e0b
Mariacele <mariacelestecaballeromamani@gmail.com>
2024-11-15 21:49:11 -0400

    Add files via upload

---
+++ datosM.js
@@ -0,0 +1,15 @@
+function z(){
+	var x=document.getElementById('a1').value;
+	 
+	var y=document.getElementById('a2').value;
+	if(x=="celeste"){
+		if(y== "carro"){
+		alert("BIENVENIDO");
+			document.location.href="principal.html"
+
+	}}
+	else
+	{
+		alert("Datos Incorrectos");
+	}
+}
